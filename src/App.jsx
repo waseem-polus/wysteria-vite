d1a0960a9dadef9c6c0145aed517c8d0f2d1f7cd
waseem-polus <waseem_imad@yahoo.com>
2023-09-26 08:35:17 -0500

    Customized App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import wuiLogo from '/wisteria.svg'
+import { Button, Container } from '@willoui/design'
 import './App.css'
 
 function App() {
@@ -8,25 +10,34 @@ function App() {
 
   return (
     <>
-      <div>
+      <Container padding="md">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <a href="https://wui-ten.vercel.app" target="_blank">
+          <img src={wuiLogo} className="logo wui" alt="WUI Logo" />
+        </a>
+      </Container>
+
+      <h1>Vite + React + WUI</h1>
+      
+      <Container direction="column" align="center" padding="xl">
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      </Container>
+
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logos to learn more
+        <br />
+        <br />
+        wisteria by Kevin from <a href="https://thenounproject.com/browse/icons/term/wisteria/" target="_blank" title="wisteria Icons">Noun Project</a> (CC BY 3.0)
       </p>
     </>
   )
