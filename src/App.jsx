bd4f49428faa864f3a9f8c9cd2650f509f1a941e
waseem-polus <waseem_imad@yahoo.com>
2023-09-26 09:27:02 -0500

    Adjusted layout spacing

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,39 @@ function App() {
 
   return (
     <>
-      <Container padding="md">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://wui-ten.vercel.app" target="_blank">
-          <img src={wuiLogo} className="logo wui" alt="WUI Logo" />
-        </a>
-      </Container>
+      <Container padding ="md" direction="column" align="center">
+        <Container padding="md">
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
 
-      <h1>Vite + React + WUI</h1>
-      
-      <Container direction="column" align="center" padding="xl">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+
+          <a href="https://wui-ten.vercel.app" target="_blank">
+            <img src={wuiLogo} className="logo wui" alt="WUI Logo" />
+          </a>
+        </Container>
+
+        <h1>Vite + React + WUI</h1>
+        
+        <Container direction="column" align="center" padding="xl">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </Container>
+
+        <p className="read-the-docs">
+          Click on the logos to learn more
         </p>
       </Container>
 
       <p className="read-the-docs">
-        Click on the logos to learn more
-        <br />
-        <br />
         wisteria by Kevin from <a href="https://thenounproject.com/browse/icons/term/wisteria/" target="_blank" title="wisteria Icons">Noun Project</a> (CC BY 3.0)
       </p>
     </>
