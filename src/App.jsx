075c2491bf690579592423370d965438e180952c
waseem-polus <waseem_imad@yahoo.com>
2023-09-26 09:36:10 -0500

    Moved react logo to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import reactLogo from "./assets/react.svg"
+import reactLogo from "/react.svg"
 import viteLogo from "/vite.svg"
 import wuiLogo from "/wisteria.svg"
 import { Button, Container } from "@willoui/design"
