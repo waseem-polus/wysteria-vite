997ea54419bc9970793d4d391781a27fcb20f115
waseem-polus <waseem_imad@yahoo.com>
2023-10-14 12:07:20 -0500

    added wysteria logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import reactLogo from "/react.svg"
 import viteLogo from "/vite.svg"
-import wuiLogo from "/wisteria.svg"
+import wuiLogo from "/wysteria.svg"
 import { Button, Container } from "@willoui/design"
 import "./App.css"
 
@@ -9,43 +9,37 @@ function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<>
-			<Container padding ="md" direction="column" align="center">
-				<Container padding="md">
-					<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-						<img src={viteLogo} className="logo" alt="Vite logo" />
-					</a>
+		<Container padding ="md" direction="column" align="center">
+			<Container padding="md">
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
 
-					<a href="https://react.dev" target="_blank" rel="noreferrer">
-						<img src={reactLogo} className="logo react" alt="React logo" />
-					</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
 
-					<a href="https://wui-ten.vercel.app" target="_blank" rel="noreferrer">
-						<img src={wuiLogo} className="logo wui" alt="WUI Logo" />
-					</a>
-				</Container>
+				<a href="https://wysteria.dev" target="_blank" rel="noreferrer">
+					<img src={wuiLogo} className="logo wui" alt="WUI Logo" />
+				</a>
+			</Container>
 
-				<h1>Vite + React + WUI</h1>
+			<h1>Vite + React + WUI</h1>
         
-				<Container direction="column" align="center" padding="xl">
-					<Button onClick={() => setCount((count) => count + 1)}>
+			<Container direction="column" align="center" padding="xl">
+				<Button onClick={() => setCount((count) => count + 1)}>
             count is {count}
-					</Button>
+				</Button>
 
-					<p>
+				<p>
             Edit <code>src/App.jsx</code> and save to test HMR
-					</p>
-				</Container>
-
-				<p className="read-the-docs">
-          Click on the logos to learn more
 				</p>
 			</Container>
 
 			<p className="read-the-docs">
-        wisteria by Kevin from <a href="https://thenounproject.com/browse/icons/term/wisteria/" target="_blank" title="wisteria Icons" rel="noreferrer">Noun Project</a> (CC BY 3.0)
+          Click on the logos to learn more
 			</p>
-		</>
+		</Container>
 	)
 }
 
